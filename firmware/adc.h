f9fc6b69b2bff922091580a60e6bea6d8e7f22d3
Daniel Zerlett <daniel@zerlett.eu>
2016-10-02 18:00:09 +0200

    move to folder "firmware"

---
+++ firmware/adc.h
@@ -0,0 +1,3 @@
+void ADC_init(void);
+uint16_t ADC_Read( uint8_t channel );
+uint16_t ADC_Read_Avg( uint8_t channel, uint8_t average );
--- stdinc.h
+++
@@ -1,7 +0,0 @@
-#pragma once
-
-#include <avr/io.h>
-#include <stdlib.h> 
-#include <string.h>
-#include <avr/interrupt.h>
-#include <avr/pgmspace.h>
